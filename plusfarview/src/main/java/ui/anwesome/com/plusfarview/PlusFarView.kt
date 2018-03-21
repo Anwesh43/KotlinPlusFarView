ca3b82229e8423cbf0ba7481866ead5418f5eb57
Anwesh43 <anweshthecool0@gmail.com>
2018-03-21 08:06:42 +0530

    adding PlusFarView class

--- plusfarview/src/main/java/ui/anwesome/com/plusfarview/PlusFarView.kt
+++ plusfarview/src/main/java/ui/anwesome/com/plusfarview/PlusFarView.kt
@@ -3,3 +3,20 @@ package ui.anwesome.com.plusfarview
 /**
  * Created by anweshmishra on 21/03/18.
  */
+import android.view.*
+import android.content.*
+import android.graphics.*
+class PlusFarView(ctx : Context) : View(ctx) {
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    override fun onDraw(canvas : Canvas) {
+
+    }
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
